559251ed4c6415379335a84222d5ba1b7096aed6
SriGuru1 <hgsriguru@gmail.com>
2026-07-01 09:41:22 +0530

    fix: auto-resolve and prefix JDBC database URLs on startup

--- src/main/java/com/urlShortener/UrlShortenerApplication.java
+++ src/main/java/com/urlShortener/UrlShortenerApplication.java
@@ -7,6 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UrlShortenerApplication {
 
 	public static void main(String[] args) {
+		String mysqlUrl = System.getenv("MYSQL_URL");
+		if (mysqlUrl != null && mysqlUrl.startsWith("mysql://")) {
+			System.setProperty("spring.datasource.url", "jdbc:" + mysqlUrl);
+		}
+
+		String databaseUrl = System.getenv("DATABASE_URL");
+		if (databaseUrl != null) {
+			if (databaseUrl.startsWith("postgres://")) {
+				System.setProperty("spring.datasource.url", "jdbc:postgresql://" + databaseUrl.substring("postgres://".length()));
+			} else if (databaseUrl.startsWith("mysql://")) {
+				System.setProperty("spring.datasource.url", "jdbc:" + databaseUrl);
+			}
+		}
+
 		SpringApplication.run(UrlShortenerApplication.class, args);
 	}
 
